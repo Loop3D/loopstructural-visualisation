4c5c3eecd03ea7ef41594464454f762218accc37
Lachlan Grose <lachlan.grose@monash.edu>
2024-10-14 08:12:42 +1100

    updating view to have visibility toggle and opacity slider

--- loopstructuralvisualisation/trame/ui/vuetify3.py
+++ loopstructuralvisualisation/trame/ui/vuetify3.py
@@ -19,7 +19,7 @@ from trame.widgets import trame
 from pyvista.trame.views import PyVistaLocalView
 from pyvista.trame.views import PyVistaRemoteLocalView
 from pyvista.trame.views import PyVistaRemoteView
-
+import pyvista
 from pyvista.trame.ui.base_viewer import BaseViewer
 from pyvista.trame.ui.vuetify3 import Viewer
 from trame.widgets.vuetify3 import VTreeview
@@ -27,8 +27,6 @@ from trame.widgets.vuetify3 import VTreeview
 
 if TYPE_CHECKING:  # pragma: no cover
     from trame_client.ui.core import AbstractLayout
-
-test = {}
 
 
 class LoopViewer(Viewer):
@@ -43,7 +41,10 @@ class LoopViewer(Viewer):
         return SinglePageWithDrawerLayout(*args, **kwargs)
 
     def ui(self, *args, **kwargs):
+        with self.layout as layout:
+            layout.title.set_text("LoopStructural Viewer")
         with self.layout.content:
+
             return super().ui(*args, **kwargs)
 
     def toggle_visibility(self, **kwargs):
@@ -52,10 +53,21 @@ class LoopViewer(Viewer):
         so we need to check the keys and update accordingly
         """
         for k in kwargs.keys():
-            object_name = k.split("__")[0]
-            test[object_name] = kwargs[k]
-            if object_name in self.plotter.objects:
-                self.plotter.objects[object_name]["actor"].visibility = kwargs[k]
+            object_name = k.split("__visibility")[0]
+            if object_name in self.plotter.actors:
+                self.plotter.actors[object_name].visibility = kwargs[k]
+        self.update()
+        # self.actors[k].visibility = not self.actors[k].visibility
+
+    def set_opacity(self, **kwargs):
+        """Set the opacity of an object in the plotter.
+        this is a slot called by the state change, the kwargs are the current state
+        so we need to check the keys and update accordingly
+        """
+        for k in kwargs.keys():
+            object_name = k.split("__opacity")[0]
+            if object_name in self.plotter.actors:
+                self.plotter.actors[object_name].prop.opacity = kwargs[k]
         self.update()
         # self.actors[k].visibility = not self.actors[k].visibility
 
@@ -63,18 +75,43 @@ class LoopViewer(Viewer):
         with self.layout.drawer as drawer:
             with vuetify.VCard():
 
-                for k, a in self.plotter.objects.items():
+                for k, a in self.plotter.actors.items():
+                    if type(a) != pyvista.plotting.actor.Actor:
+                        continue
                     drawer.server.state[f"{k}__visibility"] = True
+                    drawer.server.state[f"{k}__control_visibility"] = False
+                    drawer.server.state[f"{k}__opacity"] = a.prop.opacity
                     drawer.server.state.change(f"{k}__visibility")(self.toggle_visibility)
-
-                    with vuetify.VRow():
+                    drawer.server.state.change(f"{k}__opacity")(self.set_opacity)
+                    with vuetify.VRow(
+                        classes='pa-0 ma-0 align-center fill-height',
+                        style='flex-wrap: nowrap',
+                    ):
                         with vuetify.VCol():
 
                             vuetify.VCheckbox(
                                 label=k,
+                                classes="ma-0 pa-0",
                                 v_model=(f"{k}__visibility"),
                                 # click=(self.toggle_visibility("test")),
                             )
-                # vuetify.VCheckbox(label="Test")
-                # vuetify.VCheckbox(label="Test")
-                # vuetify.VCheckbox(label="Test")
+                        with vuetify.VCol():
+                            vuetify.VBtn(
+                                icon='mdi-dots-horizontal',
+                                click=(f'{k}__control_visibility=!{k}__control_visibility'),
+                                # click=(self.toggle_visibility("test")),
+                            )
+                    with vuetify.VCard(classes="ma-0 pa-0", v_show=f'{k}__control_visibility'):
+
+                        with vuetify.VRow(
+                            classes="ma-0 pa-0 d-flex align-center",
+                        ):
+
+                            vuetify.VSlider(
+                                v_model=(f"{k}__opacity"),
+                                label="Opacity",
+                                min=0,
+                                max=1,
+                                step=0.1,
+                                thumb_label=True,
+                            )
