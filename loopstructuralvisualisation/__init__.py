b77f7fa0b0ead91d24ab14ec1a504ff00d60321d
lachlangrose <lachlangrose@users.noreply.github.com>
2024-12-18 01:38:52 +0000

    style: style fixes by ruff and autoformatting by black

--- loopstructuralvisualisation/__init__.py
+++ loopstructuralvisualisation/__init__.py
@@ -3,8 +3,8 @@ from ._colours import random_colour
 from ._rotation_angle import RotationAnglePlotter
 from ._2d_viewer import Loop2DView
 from ._stratigraphic_column import StratigraphicColumnView
+
 try:
     from ._register_loop_ui import *  # this replaces default pyvista trame ui
 except ImportError:
     print("Could not import trame ui")
-
